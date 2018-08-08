351ccfe5ddfeac2065b1bb9c7bc7b5488b852d19
ezzaldean1 <42187031+ezzaldean1@users.noreply.github.com>
2018-08-08 16:04:52 +0300

    Update ez.js

--- ez.js
+++ ez.js
@@ -13,11 +13,11 @@ client.on('ready', () => {
   console.log(' Music Bot Is Online')
   console.log('---------------')
   client.user.setStatus("Online")
-    client.user.setActivity(" Type ehelp",{type: 'WATCHING'})
+    client.user.setActivity(" Type ezhelp",{type: 'WATCHING'})
 	
 });
 	
-            var prefix = "E" ;
+            var prefix = "ez" ;
 	
 	
 	/*
@@ -31,7 +31,7 @@ npm install queue
 */
 client.on('message', msg => {
 
-    if (msg.content == 'Eplay') {
+    if (msg.content == 'ezplay') {
         if (msg.member.voiceChannel) {
 
      if (msg.member.voiceChannel.joinable) {
